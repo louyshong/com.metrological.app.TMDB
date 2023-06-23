d4ae4e4a1564cb872c4815b750116e3c6edf36fb
Louys Hong <louyshong1998@gmail.com>
2023-06-23 22:50:25 +0800

    Render static compressed textures for testing

--- src/lib/tools.js
+++ src/lib/tools.js
@@ -1,3 +1,11 @@
 export const getImgUrl = (imgPath, width = 185)=> {
-    return `//image.tmdb.org/t/p/w${width}${imgPath}`
+    switch (width) {
+        case 1280: 
+            return "images/background1280.pvr"
+        case 500:
+            return "images/poster500.pvr"
+        case 300:
+            return "images/poster300.pvr"
+    }
+    // return `//image.tmdb.org/t/p/w${width}${imgPath}`
 };
